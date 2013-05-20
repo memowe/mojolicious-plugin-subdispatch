ca43d93bac67418bff9d5f3520667da74f991bf6
Mirko Westermeier <mirko@westermeier.de>
2013-05-20 14:30:35 +0200

    prepared release

--- lib/Mojolicious/Plugin/Subdispatch.pm
+++ lib/Mojolicious/Plugin/Subdispatch.pm
@@ -3,7 +3,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 
 use Mojo::UserAgent::Transactor;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 has app         => sub { die 'not registered!' };
 has transactor  => sub { Mojo::UserAgent::Transactor->new };
